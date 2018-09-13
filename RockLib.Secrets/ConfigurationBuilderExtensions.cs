abdb83c8c615d8b96a2d0ecb0a855ff96bc9b094
Brian Friesen <brianfriesen@quickenloans.com>
2018-09-13 12:04:38 -0400

    API improvements

--- RockLib.Secrets/ConfigurationBuilderExtensions.cs
+++ RockLib.Secrets/ConfigurationBuilderExtensions.cs
@@ -1,13 +1,32 @@
 ﻿using Microsoft.Extensions.Configuration;
+using RockLib.Configuration.ObjectFactory;
+using System.Collections.Generic;
 
 namespace RockLib.Secrets
 {
     public static class ConfigurationBuilderExtensions
     {
-        public static void AddRockLibSecrets(this IConfigurationBuilder builder, string sectionName = "RockLib.Secrets") =>
-            builder.Add(new SecretsConfigurationSource(builder.Build(), sectionName));
+        public const string DefaultSectionName = "RockLib.Secrets";
 
-        public static void AddRockLibSecrets(this IConfigurationBuilder builder, ISecretsProvider secretsProvider) =>
+        public static IConfigurationBuilder AddRockLibSecrets(this IConfigurationBuilder builder) =>
+            builder.AddRockLibSecrets(DefaultSectionName);
+
+        public static IConfigurationBuilder AddRockLibSecrets(this IConfigurationBuilder builder, string sectionName) =>
+            builder.AddRockLibSecrets(CreateSecretsProvider(builder.Build(), sectionName));
+
+        public static IConfigurationBuilder AddRockLibSecrets(this IConfigurationBuilder builder, ISecretsProvider secretsProvider) =>
             builder.Add(new SecretsConfigurationSource(secretsProvider));
+
+        private static ISecretsProvider CreateSecretsProvider(IConfiguration configuration, string sectionName)
+        {
+            List<ISecretsProvider> secretsProviders;
+            try { secretsProviders = configuration.GetSection(sectionName).Create<List<ISecretsProvider>>(); }
+            catch { return new NullSecretsProvider(); } // TODO: or throw an exception? or don't catch the exception? 
+            if (secretsProviders.Count == 0)
+                return new NullSecretsProvider(); // TODO: or throw an exception?
+            if (secretsProviders.Count == 1)
+                return secretsProviders[0];
+            return new CompositeSecretsProvider(secretsProviders);
+        }
     }
 }
