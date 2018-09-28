c0da7ab594baf350761c63ec2ecd4ee04b1ce79d
Justin Moss <Justin.Moss@gmail.com>
2018-09-28 16:25:19 -0400

    The Aws Lambda example of Aws Secrets

---
+++ Example.Secrets.Aws.Lambda/Function.cs
@@ -0,0 +1,36 @@
+using System;
+using Amazon.Lambda.Core;
+using Microsoft.Extensions.Configuration;
+using RockLib.Configuration;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace Example.Secrets.Aws.Lambda
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public void FunctionHandler(string input, ILambdaContext context)
+        {
+            // Grabbing old configuration before calling anything from RockLib.Configuration
+            var oldConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var oldSetting1 = oldConfig.GetSection("AppSettings:TestSecret1").Value;
+            var oldSetting2 = oldConfig.GetSection("AppSettings:TestSecret2").Value;
+            Console.WriteLine($"Old TestSecret1: {oldSetting1}");
+            Console.WriteLine($"Old TestSecret2: {oldSetting2}");
+
+            // This should automatically get replaced with the secret in Amazons SecretManager
+            var replacedSetting1 = Config.AppSettings["TestSecret1"];
+            var replacedSetting2 = Config.AppSettings["TestSecret2"];
+            Console.WriteLine($"New TestSecret1: {replacedSetting1}");
+            Console.WriteLine($"New TestSecret2: {replacedSetting2}");
+        }
+    }
+}
