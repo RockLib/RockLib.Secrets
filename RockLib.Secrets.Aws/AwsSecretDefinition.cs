b96f32d49901e0400d3e4b6f9847ff3f2a9728a2
Justin Moss <Justin.Moss@gmail.com>
2018-10-08 11:50:21 -0400

    Added doc comments and renamed AwsSecretId to AwsSecretName.

--- RockLib.Secrets.Aws/AwsSecretDefinition.cs
+++ RockLib.Secrets.Aws/AwsSecretDefinition.cs
@@ -1,16 +1,36 @@
 ﻿namespace RockLib.Secrets.Aws
 {
+    /// <summary>
+    /// A class that defines the necessary information for retrieving an AWS secret.
+    /// </summary>
     public class AwsSecretDefinition
     {
-        public AwsSecretDefinition(string key, string awsSecretId, string awsSecretKey)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AwsSecretDefinition"/> class.
+        /// </summary>
+        /// <param name="key">The key used to retrieve the secret from the provider.</param>
+        /// <param name="awsSecretName">The name of the secret in AWS.</param>
+        /// <param name="awsSecretKey">The key of the secret in AWS.</param>
+        public AwsSecretDefinition(string key, string awsSecretName, string awsSecretKey)
         {
             Key = key;
-            AwsSecretId = awsSecretId;
+            AwsSecretName = awsSecretName;
             AwsSecretKey = awsSecretKey;
         }
 
+        /// <summary>
+        /// Gets the key used to retrieve the secret from the provider.
+        /// </summary>
         public string Key { get; }
-        public string AwsSecretId { get; }
+
+        /// <summary>
+        /// Gets the name of the secret in AWS.
+        /// </summary>
+        public string AwsSecretName { get; }
+
+        /// <summary>
+        /// Gets the key of the secret in AWS.
+        /// </summary>
         public string AwsSecretKey { get; }
     }
 }
