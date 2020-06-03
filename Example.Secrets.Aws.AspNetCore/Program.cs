7d551d9b08a64fbebf8eaa501b46508f2318c577
Brian Friesen <brianfriesen@quickenloans.com>
2020-06-03 14:55:16 -0400

    Add AspNetCore example project, remove lambda example
    Co-authored-by: Justin Moss <Justin.Moss@gmail.com>
    Co-authored-by: Ian Kwiotek <iankwiotek@comcast.net>

---
+++ Example.Secrets.Aws.AspNetCore/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RockLib.Secrets;
+using RockLib.Secrets.Aws;
+
+namespace Example.Secrets.Aws.AspNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var credentialsFile = new NetSDKCredentialsFile();
+
+            //credentialsFile.TryGetProfile("default", out var profile);
+
+            //profile.Options.AccessKey = "ASIASRHLYM45QDLNWQMM";
+            //profile.Options.SecretKey = "V62p4N2YVMIoMPaI03N4coeiPUqHIlVzxJNBSl1w";
+            //profile.Options.Token = "FwoGZXIvYXdzEBQaDMY86r7wFEx4zNcIMCKAAjzyzR2UviCqnEuszVsDyZVYJ+seTAXRS/2zFVMOLW+EHd90UIzYWq+1yqISzKPmuIem1rw5KjEAInMzDWG2bPb7mQO3DBMr+jeB/Yk88fPh4XoojXSm6/8onbiJcWbmyDGhXNHtaURoDnsiCRitGZCnifgtAxVVQVIXpCTlE320HF4RkuBPaUoikJQB45wDyiSPEw1rEwmBZt3kP0YsiZsDKr2l39eQqg6gwhD2HRn+zJrTAs5ZArO06KHgk82t7WQRSrzPwIG62lzTkfZ7h9HGiaRnaJ2lAeCphjY9oDtwmis7OPPFyqTyH7pFMK7Hw97MxKaHzrRDXAZJTF4q7jIo8KXc9gUyKxPVCguA37Srd0D3PuwvMT1aNeurGdqR3buo7sYhfxeMzSkau7Wp70SlvgI=";
+            //credentialsFile.RegisterProfile(profile);
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                    builder.SetSecretExceptionHandler(context => Console.WriteLine(context.Exception))
+                        .AddRockLibSecrets()
+                            /*.AddSecret(new AwsSecret("WeatherUndergroundApiKey", "WeatherUnderground", "ApiKey"))*/)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
