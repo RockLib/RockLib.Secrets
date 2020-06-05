62fd3c7436c886b6c47f7e306d74a99db03fd070
Justin Moss <justin.moss@gmail.com>
2020-06-05 16:26:08 -0400

    Add AddAwsSecret extension for ISecretsConfigurationBuilder

---
+++ RockLib.Secrets.Aws.Tests/AwsSecretsConfigurationBuilderExtensionsTests .cs
@@ -0,0 +1,35 @@
+﻿using Amazon.SecretsManager;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace RockLib.Secrets.Aws.Tests
+{
+    partial class AwsSecretTests
+    {
+        [Fact(DisplayName = "AddAwsSecret calls AddSecret with AwsSecret with all passed in values")]
+        public void AddAwsSecretHappyPath()
+        {
+            ISecret secret = null;
+            var builderMock = new Mock<ISecretsConfigurationBuilder>();
+            builderMock
+                .Setup(bm => bm.AddSecret(It.IsAny<ISecret>()))
+                .Callback<ISecret>(s => secret = s);
+
+            var key = "key";
+            var awsSecretName = "awsSecretName";
+            var awsSecretKey = "aswSecretKey";
+            var awsSecretsManager = new Mock<IAmazonSecretsManager>().Object;
+
+            builderMock.Object.AddAwsSecret(key, awsSecretName, awsSecretKey, awsSecretsManager);
+
+            builderMock.Verify(bm => bm.AddSecret(It.IsAny<ISecret>()), Times.Once);
+
+            var awsSecret = secret.Should().BeOfType<AwsSecret>().Subject;
+            awsSecret.Key.Should().Be(key);
+            awsSecret.AwsSecretName.Should().Be(awsSecretName);
+            awsSecret.AwsSecretKey.Should().Be(awsSecretKey);
+            awsSecret.SecretsManager.Should().BeSameAs(awsSecretsManager);
+        }
+    }
+}
---
+++ RockLib.Secrets.Aws/AwsSecretsConfigurationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.SecretsManager;
+
+namespace RockLib.Secrets.Aws
+{
+    /// <summary>
+    /// Extension methods for secrets configuration builders related to AWS.
+    /// </summary>
+    public static class AwsSecretsConfigurationBuilderExtensions
+    {
+        /// <summary>
+        /// Adds an <see cref="AwsSecret"/> to the <see cref="ISecretsConfigurationBuilder"/>.
+        /// </summary>
+        /// <param name="builder">The builder the secret is beind added to.</param>
+        /// <param name="key">The key used to retrieve the secret from the provider.</param>
+        /// <param name="awsSecretName">The name of the secret in AWS.</param>
+        /// <param name="awsSecretKey">The key of the secret in AWS.</param>
+        /// <param name="secretsManager">The <see cref="IAmazonSecretsManager"/> client used for routing calls to AWS.</param>
+        /// <returns>The same <see cref="ISecretsConfigurationBuilder"/>.</returns>
+        public static ISecretsConfigurationBuilder AddAwsSecret(this ISecretsConfigurationBuilder builder, string key, string awsSecretName, string awsSecretKey = null, IAmazonSecretsManager secretsManager = null) => 
+            builder.AddSecret(new AwsSecret(key, awsSecretName, awsSecretKey, secretsManager));
+    }
+}
