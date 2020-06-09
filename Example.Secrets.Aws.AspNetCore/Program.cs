6e9b618d48ac04e01653f55476b0781bc28594a4
Brian Friesen <brianfriesen@quickenloans.com>
2020-06-09 16:49:13 -0400

    Remove commented-out code
    Co-authored-by: Justin Moss <Justin.Moss@gmail.com>
    Co-authored-by: Ian Kwiotek <IanKwiotek@quickenloans.com>

--- Example.Secrets.Aws.AspNetCore/Program.cs
+++ Example.Secrets.Aws.AspNetCore/Program.cs
@@ -9,17 +9,7 @@ namespace Example.Secrets.Aws.AspNetCore
     {
         public static void Main(string[] args)
         {
-            //var credentialsFile = new NetSDKCredentialsFile();
-
-            //credentialsFile.TryGetProfile("default", out var profile);
-
-            //profile.Options.AccessKey = "ASIASRHLYM45QDLNWQMM";
-            //profile.Options.SecretKey = "V62p4N2YVMIoMPaI03N4coeiPUqHIlVzxJNBSl1w";
-            //profile.Options.Token = "FwoGZXIvYXdzEBQaDMY86r7wFEx4zNcIMCKAAjzyzR2UviCqnEuszVsDyZVYJ+seTAXRS/2zFVMOLW+EHd90UIzYWq+1yqISzKPmuIem1rw5KjEAInMzDWG2bPb7mQO3DBMr+jeB/Yk88fPh4XoojXSm6/8onbiJcWbmyDGhXNHtaURoDnsiCRitGZCnifgtAxVVQVIXpCTlE320HF4RkuBPaUoikJQB45wDyiSPEw1rEwmBZt3kP0YsiZsDKr2l39eQqg6gwhD2HRn+zJrTAs5ZArO06KHgk82t7WQRSrzPwIG62lzTkfZ7h9HGiaRnaJ2lAeCphjY9oDtwmis7OPPFyqTyH7pFMK7Hw97MxKaHzrRDXAZJTF4q7jIo8KXc9gUyKxPVCguA37Srd0D3PuwvMT1aNeurGdqR3buo7sYhfxeMzSkau7Wp70SlvgI=";
-            //credentialsFile.RegisterProfile(profile);
-
             CreateHostBuilder(args).Build().Run();
-
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
