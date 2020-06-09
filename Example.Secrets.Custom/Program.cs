b71bdc36db0694a973d018bdcd1d917e606b9729
Brian Friesen <brianfriesen@quickenloans.com>
2020-06-09 16:48:55 -0400

    Custom secrets example shows both ways of defining secrets
    Co-authored-by: Justin Moss <Justin.Moss@gmail.com>
    Co-authored-by: Ian Kwiotek <IanKwiotek@quickenloans.com>

--- Example.Secrets.Custom/Program.cs
+++ Example.Secrets.Custom/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using RockLib.Secrets;
 using System;
+using System.Diagnostics;
 
 namespace Example.Secrets.Custom
 {
@@ -8,18 +9,23 @@ namespace Example.Secrets.Custom
     {
         public static void Main(string[] args)
         {
-            var configBuilder = new ConfigurationBuilder();
+            // This example defines two secrets to be added to configuration. The 'SomeApiKey'
+            // secret is added programmatically and the 'MyConnectionString' secret is defined
+            // in the appsettings.json file under the 'RockLib.Secrets' section.
+
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder();
 
             configBuilder.AddJsonFile("appsettings.json");
-            configBuilder.AddRockLibSecrets();
+            
+            configBuilder.AddRockLibSecrets()
+                .AddSecret(new ReversedSecret("SomeApiKey", "terces dnoces eht si sihT"));
 
-            var config = configBuilder.Build();
+            IConfiguration config = configBuilder.Build();
 
-            Console.WriteLine("First Secret: " + config["FirstSecretKey"]);
-            Console.WriteLine("Second Secret: " + config["SecondSecretKey"]);
-            Console.WriteLine("Third Secret: " + config["ThirdSecretKey"]);
+            Console.WriteLine("MyConnectionString: " + config["MyConnectionString"]);
+            Console.WriteLine("SomeApiKey: " + config["SomeApiKey"]);
 
-            Console.ReadKey();
+            Debugger.Break();
         }
     }
 }
